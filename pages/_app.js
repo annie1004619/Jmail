fbdb55038e9c3b02be0315beea28e0897d870ec7
annie1004619 <annie1004619@naver.com>
2021-07-21 16:39:51 +0900

    add: redux-persist

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,17 @@
 import '../styles/globals.css'
+import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
+import {persistStore} from "redux-persist";
 import {store} from '../stores/index'
-import {Provider} from "react-redux";
 
 function MyApp({Component, pageProps}) {
+    const persistor = persistStore(store)
+
     return (
         <Provider store={store}>
-            <Component {...pageProps} />
+            <PersistGate loading={null} persistor={persistor}>
+                <Component {...pageProps} />
+            </PersistGate>
         </Provider>
     )
 }
--- stores/index.js
+++ stores/index.js
@@ -1,7 +1,21 @@
 import {configureStore, getDefaultMiddleware} from '@reduxjs/toolkit';
 import {createWrapper} from "next-redux-wrapper";
 import {reducer} from '../lib/slices/index'
+import {persistStore, persistReducer} from "redux-persist";
+import storage from 'redux-persist/lib/storage'
+
+const persistConfig = {
+    key: 'root',
+    storage
+}
+const defaultMiddleware = getDefaultMiddleware({
+    serializableCheck: false,
+});
+
+const enhancedReducer = persistReducer(persistConfig, reducer);
 
 export const store = configureStore({
-    reducer,
+    reducer: enhancedReducer,
+    middleware: defaultMiddleware
 });
+
