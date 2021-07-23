c46807bd3d4d6ba1df636a18810090021919f3b0
annie1004619 <annie1004619@naver.com>
2021-07-23 10:13:32 +0900

    fix:composeModal, login, reply

--- components/mailDetail/Reply.js
+++ components/mailDetail/Reply.js
@@ -1,99 +1,133 @@
-import React from 'react';
-import {useDispatch, useSelector} from "react-redux";
-import {MdArrowDropDown, MdReply, MdDelete} from "react-icons/md";
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { MdArrowDropDown, MdReply, MdDelete } from "react-icons/md";
 import useInput from "../../hooks/useInput";
-import {v4 as uuidv4} from 'uuid';
-import {addMail} from "../../lib/slices/mailSlice";
-import {addThreadMails} from "../../lib/slices/threadSlice";
-import {addInbox, addSent} from "../../lib/slices/usersSlice";
-import {addLoginSent} from "../../lib/slices/loginUserSlice";
+import { v4 as uuidv4 } from "uuid";
+import { addMail } from "../../lib/slices/mailSlice";
+import { addThreadMails } from "../../lib/slices/threadSlice";
+import { addInbox, addMyMails, addSent } from "../../lib/slices/usersSlice";
+import { addLoginMyMails, addLoginSent } from "../../lib/slices/loginUserSlice";
 
-function Reply({sender, setReply, body, threadId}) {
-    const date = new Date();
-    const dispatch = useDispatch();
-    const loginUser = useSelector(state => state.loginUser.user)
-    const userList = useSelector(state => state.users.users)
+function Reply({ sender, setReply, body, threadId }) {
+  const date = new Date();
+  const dispatch = useDispatch();
+  const userIndex = useSelector((state) => state.loginUser.index);
+  const loginUser = useSelector((state) => state.loginUser.user);
+  const userList = useSelector((state) => state.users.users);
 
-    const [content, onChangeContent, setContent] = useInput('')
+  const [content, onChangeContent, setContent] = useInput("");
 
-    const onSend = () => {
-        if (!loginUser) return;
-        if (!userList) return;
+  const onSend = () => {
+    if (!loginUser) return;
+    if (!userList) return;
 
-        const id = uuidv4()
+    const id = uuidv4();
 
-        const receiverArr = [sender.email]
+    const receiverArr = [sender.email];
 
-        const mailObj = {
-            [id]: {
-                id: id,
-                sender: loginUser,
-                date: date.getTime() / 1000.0,
-                body: {
-                    title: `RE: ${body.title}`,
-                    content
-                },
-                receiver: receiverArr
-            }
-        }
-        dispatch(addMail(mailObj))
-        dispatch(addThreadMails({key: threadId, id: id}))
+    const mailObj = {
+      [id]: {
+        id: id,
+        sender: loginUser,
+        date: date.getTime() / 1000.0,
+        body: {
+          title: `RE: ${body.title}`,
+          content,
+        },
+        receiver: receiverArr,
+      },
+    };
+    dispatch(addMail(mailObj));
+    dispatch(addThreadMails({ key: threadId, id: id, sender: loginUser.name }));
 
-        userList.map(({email}, index) => {
-            receiverArr.map((item) => {
-                if (email === item) {
-                    dispatch(addInbox({
-                        index: index, id: {
-                            [threadId]: false
-                        }
-                    }))
-                    dispatch(addSent({
-                        index: index, id: {
-                            [threadId]: [id]
-                        }
-                    }))
-                    dispatch(addLoginSent({[threadId]: [id]}))
-                    return
-                }
+    userList.map(({ email }, index) => {
+      receiverArr.map((item) => {
+        if (email === item) {
+          dispatch(
+            addInbox({
+              index,
+              id: threadId,
             })
-        })
+          );
+          dispatch(
+            addMyMails({
+              index,
+              threadId,
+              item: {
+                [threadId]: {
+                  isRead: false,
+                  isStar: false,
+                  isImportant: false,
+                },
+              },
+            })
+          );
+        }
+      });
+    });
 
-        setContent('')
-        setReply(false)
-    }
-    return (
-        <div className="flex items-start truncate">
-            <div style={{width: '50px'}}>
-                <div
-                    style={{backgroundColor: `${loginUser.color}`}}
-                    className={`rounded-full w-10 h-10 flex items-center justify-center text-sm font-semiBold text-white ml-2 mt-5 cursor-pointer`}>
-                    {loginUser.nickName}
-                </div>
-            </div>
-            <div className="flex flex-col mx-3 w-11/12 shadow-xl rounded-lg px-6 py-5">
-                <div className="flex items-center text-gray-600">
-                    <MdReply size={20} className="mr-1 cursor-pointer"/>
-                    <MdArrowDropDown size={20} className="cursor-pointer mr-3"/>
-                    <div className="mr-2 text-sm">받는 사람</div>
-                    <div
-                        className="flex items-center border rounded-full px-2 py-1 text-sm">{sender.name} {sender.email}</div>
-                </div>
-                <textarea className="resize-none h-24 outline-none mt-5" value={content} onChange={onChangeContent}/>
-                <div className="flex items-center justify-between">
-                    <button
-                        className="rounded-md w-24 h-8 bg-blue-700 text-white text-sm justify-center hover:bg-blue-600"
-                        onClick={onSend}
-                    >
-                        보내기
-                    </button>
-                    <div className="text-gray-600 cursor-pointer hover:bg-gray-100 rounded-full px-2 py-2"
-                         onClick={() => setReply(false)}>
-                        <MdDelete size={20}/>
-                    </div>
-                </div>
-            </div>
-        </div>
+    dispatch(
+      addLoginMyMails({
+        threadId,
+      })
     );
+    dispatch(
+      addMyMails({
+        index: userIndex,
+        threadId,
+      })
+    );
+    dispatch(
+      addSent({
+        index: userIndex,
+        id: threadId,
+      })
+    );
+    dispatch(addLoginSent(threadId));
+    setContent("");
+    setReply(false);
+  };
+  return (
+    <div className="flex items-start truncate">
+      <div style={{ width: "50px" }}>
+        <div
+          style={{ backgroundColor: `${loginUser.color}` }}
+          className={`rounded-full w-10 h-10 flex items-center justify-center text-sm font-semiBold text-white ml-2 mt-5 cursor-pointer`}
+        >
+          {loginUser.nickName}
+        </div>
+      </div>
+      <div className="flex flex-col mx-3 w-11/12 shadow-xl rounded-lg px-6 py-5">
+        <div className="flex items-center text-gray-600">
+          <MdReply size={20} className="mr-1 cursor-pointer" />
+          <MdArrowDropDown size={20} className="cursor-pointer mr-3" />
+          <div className="mr-2 text-sm">받는 사람</div>
+          <div className="flex items-center border rounded-full px-2 py-1 text-sm">
+            {sender.name} {sender.email}
+          </div>
+        </div>
+        <textarea
+          className="resize-none h-24 outline-none mt-5"
+          value={content}
+          onChange={onChangeContent}
+        />
+        <div className="flex items-center justify-between">
+          <button
+            className="rounded-md w-24 h-8 bg-blue-700 text-white text-sm justify-center hover:bg-blue-600"
+            onClick={onSend}
+          >
+            보내기
+          </button>
+          <div
+            className="text-gray-600 cursor-pointer hover:bg-gray-100 rounded-full px-2 py-2"
+            onClick={() => setReply(false)}
+          >
+            <MdDelete size={20} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Reply;
--- components/modal/ComposeModal.js
+++ components/modal/ComposeModal.js
@@ -1,98 +1,160 @@
-import React from 'react';
-import {MdClear} from 'react-icons/md'
-import {useDispatch, useSelector} from "react-redux";
-import useInput from '../../hooks/useInput'
-import {v4 as uuidv4} from 'uuid';
-import {addInbox, addSent} from "../../lib/slices/usersSlice";
-import {addMail} from "../../lib/slices/mailSlice";
-import {addThread} from "../../lib/slices/threadSlice";
-import {addLoginSent} from "../../lib/slices/loginUserSlice";
+import React from "react";
+import { MdClear } from "react-icons/md";
+import { useDispatch, useSelector } from "react-redux";
+import useInput from "../../hooks/useInput";
+import { v4 as uuidv4 } from "uuid";
+import { addInbox, addMyMails, addSent } from "../../lib/slices/usersSlice";
+import { addMail } from "../../lib/slices/mailSlice";
+import { addThread } from "../../lib/slices/threadSlice";
+import { addLoginMyMails, addLoginSent } from "../../lib/slices/loginUserSlice";
 
-function ComposeModal({closeModal}) {
-    const loginUser = useSelector(state => state.loginUser.user)
-    const userList = useSelector(state => state.users.users)
-    const date = new Date();
-    const dispatch = useDispatch();
+function ComposeModal({ closeModal }) {
+  const userIndex = useSelector((state) => state.loginUser.index);
+  const loginUser = useSelector((state) => state.loginUser.user);
+  const userList = useSelector((state) => state.users.users);
+  const date = new Date();
+  const dispatch = useDispatch();
 
-    const [receiver, onChangeReceiver, setReceiver] = useInput('')
-    const [title, onChangeTitle, setTitle] = useInput('')
-    const [content, onChangeContent, setContent] = useInput('')
+  const [receiver, onChangeReceiver, setReceiver] = useInput("");
+  const [title, onChangeTitle, setTitle] = useInput("");
+  const [content, onChangeContent, setContent] = useInput("");
 
-    const onSend = () => {
-        if (!loginUser) return;
-        if (!userList) return;
+  const onSend = () => {
+    const id = uuidv4();
+    const receiverArr = receiver.split(",");
 
-        const id = uuidv4()
-        const receiverArr = receiver.split(',')
+    const mailObj = {
+      [id]: {
+        id: id,
+        sender: loginUser,
+        date: date.getTime() / 1000.0,
+        body: {
+          title,
+          content,
+        },
+        receiver: receiverArr,
+      },
+    };
+    dispatch(addMail(mailObj));
 
-        const mailObj = {
-            [id]: {
-                id: id,
-                sender: loginUser,
-                date: date.getTime() / 1000.0,
-                body: {
-                    title,
-                    content
+    const threadObj = {
+      [id]: {
+        title: title,
+        sender: [loginUser.name],
+        mails: [id],
+      },
+    };
+    dispatch(addThread(threadObj));
+
+    userList.map(({ email }, index) => {
+      receiverArr.map((item) => {
+        if (email === item) {
+          dispatch(
+            addMyMails({
+              index,
+              threadId: id,
+              item: {
+                [id]: {
+                  isRead: false,
+                  isStar: false,
+                  isImportant: false,
                 },
-                receiver: receiverArr
-            }
-        }
-        dispatch(addMail(mailObj))
-
-        const threadObj = {
-            [id]: {
-                title: title,
-                mails: [id]
-            }
-
-        }
-        dispatch(addThread(threadObj))
-
-        userList.map(({email}, index) => {
-            receiverArr.map((item) => {
-                if (email === item) {
-                    dispatch(addInbox({
-                        index: index, id: {
-                            [id]: false
-                        }
-                    }))
-                    dispatch(addSent({
-                        index: index, id: {
-                            [id]: [id]
-                        }
-                    }))
-                    dispatch(addLoginSent({[id]: [id]}))
-                }
+              },
             })
-        })
-        
-        alert('메일을 보냈습니다')
-        setTitle('')
-        setContent('')
-        setReceiver('')
-        closeModal();
-
-    }
-
-    return (
-        <div className="w-400px absolute bottom-0 right-0 shadow-lg bg-white">
-            <div className="h-5 flex items-center bg-brown px-6 py-5 justify-between rounded-t-xl">
-                <div className="text-white text-sm">새 메일</div>
-                <MdClear size={20} color="#fff" className="cursor-pointer" onClick={closeModal}/>
-            </div>
-            <div className="flex flex-col">
-                <input type="text" placeholder="받는 사람" className="text-sm mx-6 border-b py-2 outline-none"
-                       value={receiver} onChange={onChangeReceiver}/>
-                <input type="text" placeholder="제목" className="text-sm mx-6 border-b py-2 outline-none" value={title}
-                       onChange={onChangeTitle}/>
-                <textarea className="mx-6 resize-none h-44 outline-none" value={content} onChange={onChangeContent}/>
-                <button
-                    className="mx-6 my-6 flex items-center rounded-md w-24 h-8 bg-blue-700 text-white text-sm justify-center hover:bg-blue-600"
-                    onClick={onSend}>보내기
-                </button>
-            </div>
-        </div>
+          );
+          dispatch(addInbox({ index, id }));
+        }
+      });
+    });
+    dispatch(
+      addLoginMyMails({
+        threadId: id,
+        item: {
+          [id]: {
+            isRead: false,
+            isStar: false,
+            isImportant: false,
+          },
+        },
+      })
     );
+    dispatch(
+      addMyMails({
+        index: userIndex,
+        threadId: id,
+        item: {
+          [id]: {
+            isRead: false,
+            isStar: false,
+            isImportant: false,
+          },
+        },
+      })
+    );
+    dispatch(
+      addSent({
+        index: userIndex,
+        id,
+      })
+    );
+    dispatch(addLoginSent(id));
+    alert("메일을 보냈습니다");
+    setTitle("");
+    setContent("");
+    setReceiver("");
+    closeModal();
+  };
+
+  return (
+    <div className="w-400px absolute bottom-0 right-0 shadow-lg bg-white">
+      <div className="h-5 flex items-center bg-brown px-6 py-5 justify-between rounded-t-xl">
+        <div className="text-white text-sm">새 메일</div>
+        <MdClear
+          size={20}
+          color="#fff"
+          className="cursor-pointer"
+          onClick={closeModal}
+        />
+      </div>
+      <div className="flex flex-col">
+        <input
+          type="text"
+          placeholder="받는 사람"
+          className="text-sm mx-6 border-b py-2 outline-none"
+          value={receiver}
+          onChange={onChangeReceiver}
+        />
+        <input
+          type="text"
+          placeholder="제목"
+          className="text-sm mx-6 border-b py-2 outline-none"
+          value={title}
+          onChange={onChangeTitle}
+        />
+        <textarea
+          className="mx-6 resize-none h-44 outline-none"
+          value={content}
+          onChange={onChangeContent}
+        />
+        {userList && loginUser && userList ? (
+          <button
+            className="mx-6 my-6 flex items-center rounded-md w-24 h-8 bg-blue-700 text-white text-sm justify-center hover:bg-blue-600"
+            onClick={onSend}
+          >
+            보내기
+          </button>
+        ) : (
+          <button
+            className="mx-6 my-6 flex items-center rounded-md w-24 h-8 bg-blue-700 text-white text-sm justify-center hover:bg-blue-600"
+            onClick={onSend}
+            disabled={true}
+          >
+            보내기
+          </button>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default ComposeModal;
