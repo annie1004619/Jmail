e6d35d559dcc46dfe21db852bbe6212450e85fc3
annie1004619 <annie1004619@naver.com>
2021-07-23 10:13:44 +0900

    fix: tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,31 @@
 module.exports = {
-    mode: 'jit',
-    purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-        extend: {
-            boxShadow: {
-                btn: '0 1px 2px 0 rgba(60, 64, 67, 0.3),' +
-                    '0 1px 3px 1px rgba(60, 64, 67, 0.15)'
-            },
-            width: {
-                '600px': '600px',
-                '238px':'238px',
-                '400px' : '400px'
-            },
-            backgroundColor: {
-                'brown': '#404040'
-            }
-
-        },
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      boxShadow: {
+        btn:
+          "0 1px 2px 0 rgba(60, 64, 67, 0.3)," +
+          "0 1px 3px 1px rgba(60, 64, 67, 0.15)",
+        item:
+          "inset 1px 0 0 #dadce0," +
+          "inset -1px 0 0 #dadce0," +
+          "0 1px 2px 0 rgba(60, 64, 67, 0.3)," +
+          "0 1px 3px 1px rgba(60, 64, 67, 0.15)",
+      },
+      width: {
+        "600px": "600px",
+        "238px": "238px",
+        "400px": "400px",
+      },
+      backgroundColor: {
+        brown: "#404040",
+      },
     },
-    variants: {
-        extend: {},
-    },
-    plugins: [],
-}
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
